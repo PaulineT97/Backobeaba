9c08ee1b4da9c84b00d35fa768c0a96bab2df1a4
Pauline Todeschini <pauline.todeschini@gmail.com>
2023-12-22 11:50:33 +0100

    test X-Requested-With

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ app.use(bodyParser.json());
 app.use(cookie());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://frontobeaba.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", 'X-Requested-With,content-type');
+  res.header("Access-Control-Allow-Credentials", true);
   next();
 });
 
